fa6796385c7e39966e41a709da94cfd77ad623b9
Matěj Hyška <matej.hyska.020@pslib.cz>
2022-05-10 08:45:05 +0200

    basic page, confirm

--- BattleShips/scripts/main.js
+++ BattleShips/scripts/main.js
@@ -2,6 +2,11 @@ function init(){
     //vars
     let cells = document.getElementsByClassName("cell");
     let grid = document.getElementById("grid-container");
+    let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    let confirm = document.getElementById("confirm");
+    let btn_yes = document.getElementById("conf_yes");
+    let btn_no = document.getElementById("conf_no");
+    let conf_text = documnt.getElementById("conf_text");
 
 
 
@@ -9,20 +14,40 @@ function init(){
 
 
     //actions
+    for (let index = 0; index < 10; index++) {
+        let letter = letters[index];
+        for (let i = 1; i < 11; i++) {
+            let element = document.createElement("div");
+            element.classList.add("cell");
+            element.innerText = letter + i;
+            element.id = letter + i;
+            grid.appendChild(element);
+        }
+    }
+
     for (let i = 0; i < cells.length; i++) {
         cells[i].addEventListener("click", e => {
-            alert("You just clicked " + e.target.id)
+            confirm.style.right = 20 + "%";
+            conf_text.innerText = "Confirm: " + e.target.id;
+            btn_yes.addEventListener("click", element => {
+                alert("You just clicked" + e.target.id);
+                confirm.style.right = 3 + "%";
+            })
+            btn_no.addEventListener("click", element => {
+                confirm.style.right = 3 + "%";
+            })
         })
     }
-    console.log(cells)
 
-    for (let index = 0; index < 15; index++) {
+/*
+    for (let index = 0; index < 100; index++) {
         let element = document.createElement("div");
         element.classList.add("cell");
+        
         grid.appendChild(element);
         
     }
-
+*/
 
 
 
