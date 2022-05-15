47d121e12eef42f9f23ef3e21dbb0cf9ad552a82
Matěj Hyška <matej.hyska.020@pslib.cz>
2022-05-15 16:49:15 +0200

    game finished, add: main page, lose, win, dificult

--- BattleShips/scripts/main.js
+++ BattleShips/scripts/main.js
@@ -13,6 +13,10 @@ function init(){
     let l_placing = false
     let light_placing = false
     let info = document.getElementById("info-text");
+    let enemy_grid = document.getElementById("enemy-grid");
+    let enemy_cells = document.getElementsByClassName("cell-enemy")
+    let shot_list = [];
+    let enemy_round_shots = 3;
 
 
 
@@ -21,7 +25,7 @@ function init(){
 
 
     //actions
-    console.log(window.location)
+    //console.log(window.location)
     for (let index = 0; index < 10; index++) {
         let letter = letters[index];
         for (let i = 1; i < 11; i++) {
@@ -32,6 +36,166 @@ function init(){
             grid.appendChild(element);
         }
     }
+    for (let index = 0; index < 10; index++) {
+        let letter = letters[index];
+        for (let i = 1; i < 11; i++) {
+            let element = document.createElement("div");
+            element.classList.add("cell-enemy");
+            element.innerText = letter + i;
+            element.id = "" + index + (i-1);
+            enemy_grid.appendChild(element);
+        }
+    }
+    grid.addEventListener("mouseleave", e => {
+        let hover_list = document.getElementsByClassName("cell-hover")
+        for (let i = hover_list.length; i > 0; i--) {
+            hover_list[i-1].classList.remove("cell-hover")
+        }
+    })
+    //random generování enemy lodí
+    //i lod generace
+    let i_rand = Math.floor((Math.random() * 69));
+    //console.log(i_rand)
+    let enemy_i_list = [i_rand, i_rand+10, i_rand+20, i_rand+30]
+    //console.log(enemy_i_list)
+
+    //square lod generace
+    let enemy_square_list = []
+    function gen_square(){
+        let square_rand = Math.floor((Math.random() * 89));
+        enemy_square_list = [square_rand, square_rand+1, square_rand+10, square_rand+11]
+        if(square_rand == 9 || square_rand.toString()[1] == "9"){
+            gen_square()
+        }
+        else if(HasDuplicates(enemy_i_list, enemy_square_list)){
+            gen_square()
+        }
+    }
+    gen_square();
+
+    //l lod generace
+    let enemy_l_list = []
+    function gen_l(){
+        let l_rand = Math.floor((Math.random() * 77));
+        enemy_l_list = [l_rand, l_rand+10, l_rand+20, l_rand+21, l_rand+22]
+        if(l_rand == 8 || l_rand == 9 || l_rand.toString()[1] == "8" || l_rand.toString()[1] == "9"){
+            gen_l()
+        }
+        else if(HasDuplicates(enemy_l_list, enemy_i_list) || HasDuplicates(enemy_l_list, enemy_square_list)){
+            gen_l()
+        }
+    }
+    gen_l();
+
+    //lightning lod generace
+    let enemy_light_list = []
+    function gen_light(){
+        let light_rand = Math.floor((Math.random() * 78));
+        enemy_light_list = [light_rand, light_rand+10, light_rand+11, light_rand+21]
+        if(light_rand == 9 || light_rand.toString()[1] == "9"){
+            gen_light()
+        }
+        else if(HasDuplicates(enemy_light_list, enemy_i_list) || HasDuplicates(enemy_light_list, enemy_square_list) || HasDuplicates(enemy_light_list, enemy_l_list)){
+            gen_light()
+        }
+    }
+    gen_light();
+
+    function GetIndexOf(item, list){
+        for (let i = 0; i < list.length; i++) {
+            //console.log(i)
+            if(list[i] == item){
+                //console.log(i)
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    function IsItemInList(item, list){
+        for (let index = 0; index < list.length; index++) {
+            if(item == list[index]) return true;
+        }
+        return false;
+    }
+
+    function HasDuplicates(list1, list2){
+        for (let i = 0; i < list1.length; i++) {
+            for (let j = 0; j < list2.length; j++) {
+                if(list1[i] == list2[j]) return true
+            }
+        }
+        return false;
+    }
+    
+
+
+
+    for (let i = 0; i < enemy_cells.length; i++) {
+        enemy_cells[i].addEventListener("mouseover", e => {
+            for (let index = 0; index < enemy_cells.length; index++) {
+                enemy_cells[index].classList.remove("cell-hover")
+            }
+            e.target.classList.add("cell-hover")
+        })
+        enemy_cells[i].addEventListener("click", e => {
+            if(ship_container.children.length == 0){
+                if(e.target.innerText != ""){
+                    e.target.innerText = "";
+                    e.target.classList.add("cell-ship-shot");
+                    //alert(e.target.id)
+                    //console.log(parseInt(e.target.id))
+                    //console.log(enemy_square_list)
+                    if(IsItemInList(parseInt(e.target.id), enemy_square_list)){
+                        enemy_square_list.splice(GetIndexOf(parseInt(e.target.id), enemy_square_list), 1);
+                        if(enemy_square_list.length == 0){
+                            info.innerText = "You sunk a Base Ship!"
+                        }
+                    }
+                    else if(IsItemInList(parseInt(e.target.id), enemy_i_list)){
+                        enemy_i_list.splice(GetIndexOf(parseInt(e.target.id), enemy_i_list), 1);
+                        if(enemy_i_list.length == 0){
+                            info.innerText = "You sunk a Battleship!"
+                        }
+                    }
+                    else if(IsItemInList(parseInt(e.target.id), enemy_l_list)){
+                        enemy_l_list.splice(GetIndexOf(parseInt(e.target.id), enemy_l_list), 1);
+                        if(enemy_l_list.length == 0){
+                            info.innerText = "You sunk a Carrier!"
+                        }
+                    }
+                    else if(IsItemInList(parseInt(e.target.id), enemy_light_list)){
+                        enemy_light_list.splice(GetIndexOf(parseInt(e.target.id), enemy_light_list), 1);
+                        if(enemy_light_list.length == 0){
+                            info.innerText = "You sunk a Cruiser!"
+                        }
+                    }
+                    else{
+                        e.target.classList.add("cell-miss")
+                    }
+                    if(enemy_square_list.length == 0 && enemy_i_list.length == 0 && enemy_l_list.length == 0 && enemy_light_list.length == 0){
+                        info.innerText = "You win!"
+                        window.location.href = "win.html";
+                    }
+                    //console.log(enemy_square_list)
+                    for (let i = 0; i < enemy_round_shots; i++) {
+                        enemy_shoot();
+                    }
+                }
+            }
+            else{
+                info.innerText = "You must place all your ships!"
+            }
+        })
+    }
+    enemy_grid.addEventListener("mouseleave", e => {
+        let hover_list = document.getElementsByClassName("cell-hover")
+        hover_list[0].classList.remove("cell-hover");
+    })
+
+
+
+
 
     for (let i = 0; i < cells.length; i++) {
         cells[i].addEventListener("click", function(event) {
@@ -121,78 +285,82 @@ function init(){
                     }
                 }
             }
-            else{
-                info.innerText = "";
-                if(ship_container.children.length == 0){
-                    let i_list = document.getElementsByClassName("cell-ship-i")
-                    let square_list = document.getElementsByClassName("cell-ship-square")
-                    let l_list = document.getElementsByClassName("cell-ship-l")
-                    let light_list = document.getElementsByClassName("cell-ship-light")
-                    let ship_list = document.getElementsByClassName("cell-ship")
-
-                    if(event.target.innerText != ""){
-                        //alert("You shot " + event.target.id)
-                        //console.log(event.target)
-                    }
-                    
-                    if(event.target.classList.contains("cell-ship")){
-                        event.target.classList.add("cell-ship-shot");
-                        event.target.classList.remove("cell-ship")
-                        console.log(ship_list.length);
-                        if(event.target.classList.contains("cell-ship-i")){
-                            event.target.classList.remove("cell-ship-i")
-                            console.log(i_list.length)
-                            if(i_list.length == 0){
-                                info.innerText = "You sunk a Battleship!";
-                            }
-                        }
-                        if(event.target.classList.contains("cell-ship-square")){
-                            event.target.classList.remove("cell-ship-square")
-                            console.log(square_list.length)
-                            if(square_list.length == 0){
-                                info.innerText = "You sunk a Base ship!";
-                            }
-                        }
-                        if(event.target.classList.contains("cell-ship-l")){
-                            event.target.classList.remove("cell-ship-l")
-                            console.log(l_list.length)
-                            if(l_list.length == 0){
-                                info.innerText = "You sunk a Carrier!";
-                            }
-                        }
-                        if(event.target.classList.contains("cell-ship-light")){
-                            event.target.classList.remove("cell-ship-light")
-                            console.log(light_list.length)
-                            if(light_list.length == 0){
-                                info.innerText = "You sunk a Cruiser!";
-                            }
-                        }
-                        if(ship_list.length == 0){
-                            info.innerText = "You win!"
-                            window.location.href = "win.html";
-                        }
-                    }
-                    else if(event.target.classList.contains("cell-ship-shot") == false){
-                        event.target.classList.add("cell-miss");
-                    }
-                    event.target.innerText = "";
-                }
-                else{
-                    info.innerText = "You must place down all you ships first!"
-                }
-            }
         })
+        function enemy_shoot(){
+            let shoot_rand = Math.floor((Math.random() * 99));
+            console.log(shoot_rand)
+            console.log(shot_list)
+            if(IsItemInList(shoot_rand, shot_list)){
+                enemy_shoot();
+            }
+            shot_list.push(shoot_rand);
+            if(ship_container.children.length == 0){
+                let i_list = document.getElementsByClassName("cell-ship-i")
+                let square_list = document.getElementsByClassName("cell-ship-square")
+                let l_list = document.getElementsByClassName("cell-ship-l")
+                let light_list = document.getElementsByClassName("cell-ship-light")
+                let ship_list = document.getElementsByClassName("cell-ship")
+
+                if(cells[shoot_rand].innerText != ""){
+                    //alert("You shot " + cells[shoot_rand].id)
+                    //console.log(cells[shoot_rand])
+                }
+                
+                if(cells[shoot_rand].classList.contains("cell-ship")){
+                    cells[shoot_rand].classList.add("cell-ship-shot");
+                    cells[shoot_rand].classList.remove("cell-ship")
+                    if(cells[shoot_rand].classList.contains("cell-ship-i")){
+                        cells[shoot_rand].classList.remove("cell-ship-i")
+                        if(i_list.length == 0){
+                            info.innerText = "Enemy sunk a Battleship!";
+                        }
+                    }
+                    if(cells[shoot_rand].classList.contains("cell-ship-square")){
+                        cells[shoot_rand].classList.remove("cell-ship-square")
+                        if(square_list.length == 0){
+                            info.innerText = "Enemy sunk a Base ship!";
+                        }
+                    }
+                    if(cells[shoot_rand].classList.contains("cell-ship-l")){
+                        cells[shoot_rand].classList.remove("cell-ship-l")
+                        if(l_list.length == 0){
+                            info.innerText = "Enemy sunk a Carrier!";
+                        }
+                    }
+                    if(cells[shoot_rand].classList.contains("cell-ship-light")){
+                        cells[shoot_rand].classList.remove("cell-ship-light")
+                        if(light_list.length == 0){
+                            info.innerText = "Enemy sunk a Cruiser!";
+                        }
+                    }
+                    if(ship_list.length == 0){
+                        info.innerText = "You lose!"
+                        window.location.href = "lose.html";
+                    }
+                }
+                else if(cells[shoot_rand].classList.contains("cell-ship-shot") == false){
+                    cells[shoot_rand].classList.add("cell-miss");
+                }
+                cells[shoot_rand].innerText = "";
+            }
+            else{
+                info.innerText = "You must place down all you ships first!"
+            }
+        }
+        
+        
+            
+
         cells[i].addEventListener("mouseover", function(event){
             for (let i = 0; i < cells.length; i++) {
                 cells[i].classList.remove("cell-hover")
             }
-            if(event.target.classList.contains("cell-ship-shot") == false){
-                event.target.classList.add("cell-hover")
-            }
             let eid = parseInt(event.target.id)
             let idnum2 = event.target.id[1];
-            console.log(idnum2)
-            cells[eid].classList.add("cell-hover")
+            if(ship_container.children.length != 0){
+                event.target.classList.add("cell-hover")
+                cells[eid].classList.add("cell-hover")
+            }
             if(i_placing == true){
                 if(cells[eid + 10] == undefined){
                     cells[eid - 10].classList.add("cell-hover")
